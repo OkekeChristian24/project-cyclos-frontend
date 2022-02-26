b7a4c7ee43f6741ce2e64e5540dafb747e5a32ce
MUAZU <home@Homes-MacBook-Pro.local>
2022-02-26 03:26:05 -0700

    finished working on the how it works section

--- src/Pages/Home/Reviews/ReviewsSlider.jsx
+++ src/Pages/Home/Reviews/ReviewsSlider.jsx
@@ -46,98 +46,79 @@ export default function ReviewsSlider() {
     <div className="slider__outer">
       <div className="slider">
         <Slider {...settings}>
-          <div className="slider__item">
-            <div className="slider__item-icon">
-              <img src="images/icons/revIcon.svg" alt="" />
+          {/* step 1 */}
+          <div  className="slider__item">
+        
+            <div  style={{margin:"0 auto", width:130, height:130,borderRadius:20, background:"#01062D", display:"flex", justifyContent:"center", alignItems:"center"}}>
+            <img width={100} src="images/howItWorks/search.png" alt="" />
             </div>
-            <p>
-              There are many variations of passages of Lorem Ipsum form, by
-              injected humour randomised words which slightly believable.
+            <div style={{margin:"0 auto", textAlign: "center"}} className="slider__item-info-text">
+                <h4 style={{marginTop:20}}>Search</h4>
+                <p style={{marginTop:20}} >
+            Browse our catalogue to select products for purchase.
             </p>
-            <div className="slider__item-info">
-              <div className="slider__item-info-avatar">
-                <img src="images/index/ava.png" alt="" />
               </div>
-              <div className="slider__item-info-text">
-                <h5>Adalberto Kovach</h5>
-                <p>Marketing Management</p>
-              </div>
-            </div>
           </div>
-          <div className="slider__item">
-            <div className="slider__item-icon">
-              <img src="images/icons/revIcon.svg" alt="" />
+   {/* step 2 */}
+   <div  className="slider__item">
+        
+            <div  style={{margin:"0 auto", width:130, height:130,borderRadius:20, background:"#01062D", display:"flex", justifyContent:"center", alignItems:"center"}}>
+            <img style={{margin:"0 auto"}} width={100} src="images/howItWorks/cart.png" alt="" />
             </div>
-            <p>
-              There are many variations of passages of Lorem Ipsum form, by
-              injected humour randomised words which slightly believable.
+            <div style={{margin:"0 auto", textAlign: "center"}} className="slider__item-info-text">
+                <h4 style={{marginTop:20}}>Put in Cart</h4>
+                <p style={{marginTop:20}} >
+                Add items to your cart. Don't forget the discounts!
             </p>
-            <div className="slider__item-info">
-              <div className="slider__item-info-avatar">
-                <img src="images/index/ava.png" alt="" />
               </div>
-              <div className="slider__item-info-text">
-                <h5>Adalberto Kovach</h5>
-                <p>Marketing Management</p>
-              </div>
-            </div>
           </div>
-          <div className="slider__item">
-            <div className="slider__item-icon">
-              <img src="images/icons/revIcon.svg" alt="" />
+             {/* step 3*/}
+   <div  className="slider__item">
+            <div  style={{margin:"0 auto", width:130, height:130,borderRadius:20, background:"#01062D", display:"flex", justifyContent:"center", alignItems:"center"}}>
+              <img  width={100} src="images/howItWorks/checkout.png" alt="" />
             </div>
-            <p>
-              There are many variations of passages of Lorem Ipsum form, by
-              injected humour randomised words which slightly believable.
+            <div style={{margin:"0 auto", textAlign: "center"}} className="slider__item-info-text">
+                <h4 style={{marginTop:20}}>Check out</h4>
+                <p style={{marginTop:20}} >
+            Fill up your carts and enjoy more discounts. Let's go...
             </p>
-            <div className="slider__item-info">
-              <div className="slider__item-info-avatar">
-                <img src="images/index/ava.png" alt="" />
               </div>
-              <div className="slider__item-info-text">
-                <h5>Adalberto Kovach</h5>
-                <p>Marketing Management</p>
-              </div>
-            </div>
           </div>
-          <div className="slider__item">
-            <div className="slider__item-icon">
-              <img src="images/icons/revIcon.svg" alt="" />
+
+   {/* step 4 */}
+   <div  className="slider__item">
+
+            <div  style={{margin:"0 auto", width:130, height:130,borderRadius:20, background:"#01062D", display:"flex", justifyContent:"center", alignItems:"center"}}>
+            <img style={{margin:"0 auto"}} width={100} src="images/howItWorks/payment.png"alt="" />
             </div>
-            <p>
-              There are many variations of passages of Lorem Ipsum form, by
-              injected humour randomised words which slightly believable.
+            <div style={{margin:"0 auto", textAlign: "center"}} className="slider__item-info-text">
+                <h4 style={{marginTop:20}}>Make Payment</h4>
+                <p style={{marginTop:20}} >
+           Fonalise payment and get your order delivered to you in no time
             </p>
-            <div className="slider__item-info">
-              <div className="slider__item-info-avatar">
-                <img src="images/index/ava.png" alt="" />
               </div>
-              <div className="slider__item-info-text">
-                <h5>Adalberto Kovach</h5>
-                <p>Marketing Management</p>
-              </div>
-            </div>
-          </div>
-          <div className="slider__item">
-            <div className="slider__item-icon">
-              <img src="images/icons/revIcon.svg" alt="" />
-            </div>
-            <p>
-              There are many variations of passages of Lorem Ipsum form, by
-              injected humour randomised words which slightly believable.
-            </p>
-            <div className="slider__item-info">
-              <div className="slider__item-info-avatar">
-                <img src="images/index/ava.png" alt="" />
-              </div>
-              <div className="slider__item-info-text">
-                <h5>Adalberto Kovach</h5>
-                <p>Marketing Management</p>
-              </div>
-            </div>
           </div>
         </Slider>
       </div>
     </div>
   );
 }
+
+{/* <div className="slider__item">
+<div className="slider__item-icon">
+  <img src="images/icons/revIcon.svg" alt="" />
+</div>
+<p>
+  There are many variations of passages of Lorem Ipsum form, by
+  injected humour randomised words which slightly believable.
+</p>
+<div className="slider__item-info">
+  <div className="slider__item-info-avatar">
+    <img src="images/index/ava.png" alt="" />
+  </div>
+  <div className="slider__item-info-text">
+    <h5>Adalberto Kovach</h5>
+    <p>Marketing Management</p>
+  </div>
+</div>
+</div> */}
