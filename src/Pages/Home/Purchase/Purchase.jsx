8a6385793d2bad019b6c986226a2f28f95bc75a8
MUAZU <home@Homes-MacBook-Pro.local>
2022-03-05 08:54:14 +0100

    modification on the purchase and footer section has been implemented successsfully

--- src/Pages/Home/Purchase/Purchase.jsx
+++ src/Pages/Home/Purchase/Purchase.jsx
@@ -6,15 +6,17 @@ export default function Purchase() {
       <div className="purchase__shape">
         <img src="images/purchase.png" alt="shapes" />
       </div>
+
       <div className="auto__container">
         <div className="purchase__inner">
           <div className="purchase__inner-cards">
             <div className="purchase__inner-card">
-              <div style={{background:"#D3D3D3"}} className="purchase__inner-card-icon">
+              {/* <div style={{background:"#D3D3D3"}} className="purchase__inner-card-icon">
                 <img src="images/icons/bell.svg" alt="bell" />
-              </div>
-              <h5>IOS/Android Phone</h5>
-              <div className="purchase__inner-card-rate">
+              </div> */}
+              <h5>Laptops and Tablets </h5>
+
+              {/* <div className="purchase__inner-card-rate">
                 <div className="star">
                   <div className="star__empty">
                     <img src="images/icons/starEmpty.svg" alt="starEmpty" />
@@ -26,85 +28,47 @@ export default function Purchase() {
                   </div>
                 </div>
                 <div className="num">4.2</div>
-              </div>
-
+              </div> */}
+              {/* 
               <p className="sm">
               Craving for a device with  newest technology that enables wireless charging, clear, detailed, high resolution images; thin, sleek and of lightweight, and uninteruppted 5G connectivity?
 <strong>Check out the new iPhone SE 2022 smartphone - $500,000</strong>
-              </p>
+              </p> */}
+              <img
+                style={{ width: "100%" }}
+                src="images/icons/laptop.jpg"
+                alt="bell"
+              />
             </div>
+
             <div className="purchase__inner-card">
-              <div style={{background:"#D3D3D3"}} className="purchase__inner-card-icon">
-                <img src="images/icons/bell.svg" alt="" />
-              </div>
-              <h5>Sports Car (Gadgets)</h5>
-              <div className="purchase__inner-card-rate">
-                <div className="star">
-                  <div className="star__empty">
-                    <img src="images/icons/starEmpty.svg" alt="" />
-                  </div>
-                  <div className="star__full" style={{ width: "90%" }}>
-                    <span>
-                      <img src="images/icons/starFull.svg" alt="" />
-                    </span>
-                  </div>
-                </div>
-                <div className="num">4.7</div>
-              </div>
+              <h5>Women’s Fashion</h5>
 
-              <p className="sm">
-              Going for that city tour, looking for a machine produced with greater acceleration, top speed, lighter, quicker, more agile and of good investment?
-<strong>Try the Lamborghini 2021 series - $3,300,000</strong>
-              </p>
+              <img
+                style={{ width: "100%" }}
+                src="images/icons/fashion.jpg"
+                alt="bell"
+              />
             </div>
+
             <div className="purchase__inner-card">
-              <div style={{background:"#D3D3D3"}} className="purchase__inner-card-icon">
-                <img src="images/icons/bell.svg" alt="" />
-              </div>
-              <h5>Luxury Wristwatch</h5>
-              <div className="purchase__inner-card-rate">
-                <div className="star">
-                  <div className="star__empty">
-                    <img src="images/icons/starEmpty.svg" alt="" />
-                  </div>
-                  <div className="star__full" style={{ width: "100%" }}>
-                    <span>
-                      <img src="images/icons/starFull.svg" alt="" />
-                    </span>
-                  </div>
-                </div>
-                <div className="num">5</div>
-              </div>
+              <h5>Beauty picks</h5>
 
-              <p className="sm">
-              In search of a timepiece, built-to-last, exudes class, coupled with innovative technology, ensures longevity and can be passed down to generations. 
-<strong>Try the Graff Diamonds, the Fascination Watch – $40 Million
-Don’t sleep on it. Add to Cart now.</strong>
-              </p>
+              <img
+                style={{ width: "100%" }}
+                src="images/icons/beauty.jpg"
+                alt="bell"
+              />
             </div>
-            <div style={{background:"#FFE2DC"}} className="purchase__inner-card">
-              <div style={{background:"#212121"}} className="purchase__inner-card-icon">
-                <img src="images/icons/bell.svg" alt="" />
-              </div>
-              <h5>Luxury Jewelry</h5>
-              <div className="purchase__inner-card-rate">
-                <div className="star">
-                  <div className="star__empty">
-                    <img src="images/icons/starEmpty.svg" alt="" />
-                  </div>
-                  <div className="star__full" style={{ width: "90%" }}>
-                    <span>
-                      <img src="images/icons/starFull.svg" alt="" />
-                    </span>
-                  </div>
-                </div>
-                <div className="num">4.7</div>
-              </div>
 
-              <p className="sm">
-              Thinking of the best way to express your feelings to a her?  She would say YES, to a high value, crystal-clear gem stone, made with the purest materials of precious metals.
-<strong>Try The Apollo Blue diamond earrings - $42,000,000</strong>
-              </p>
+            <div className="purchase__inner-card">
+              <h5>Babycare</h5>
+
+              <img
+                style={{ width: "100%" }}
+                src="images/icons/baby.jpg"
+                alt="bell"
+              />
             </div>
           </div>
           <div className="purchase__inner-content">
@@ -112,12 +76,13 @@ Don’t sleep on it. Add to Cart now.</strong>
               <img src="images/index/oval.svg" alt="" />
             </div>
             <h2> Procure with Crypto</h2>
-          
+
             <p>
-            Add goods to your shopping cart. Remember to take advantage of the discount!
+              Add goods to your shopping cart. Remember to take advantage of the
+              discount!
             </p>
             <a href="#" className="button primary">
-             Place Order
+              Place Order
             </a>
           </div>
         </div>
