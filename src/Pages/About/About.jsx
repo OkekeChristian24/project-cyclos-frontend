63f716c7fa42053ae9ea3e0cdc59ec34a55ee5af
MUAZU <kakupepe09@gmail.com>
2022-04-20 08:56:13 +0100

    fixes

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import Banner from "./Banner";
 import Intro from "./Intro";
 
 import Purchase from "./Purchase/Purchase";
-import Reviews from "./Reviews/Reviews";
-import Unique from "./Unique/unique";
+
 
 export default function Home() {
   return (
